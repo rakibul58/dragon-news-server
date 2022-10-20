1ec63548b14b8b7b8585916f977fb37344cfd083
rakibul58 <rhrahi14@gmail.com>
2022-10-20 19:11:51 +0600

    added day 2

--- index.js
+++ index.js
@@ -5,17 +5,39 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 
-app.get('/',(req , res)=>{
+app.get('/', (req, res) => {
     res.send('News API Running');
 });
 
-app.get('/news-categories' , (req , res)=>{
+app.get('/news-categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+});
+
+app.get('/news' , (req , res) =>{
+    res.send(news);
 })
 
-app.listen(port , ()=>{
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+});
+
+app.listen(port, () => {
     console.log('Dragon News Server Running on port', port);
 })
